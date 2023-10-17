7685e1b1d39c04a4b41b2a65aa0993c37ad27cf6
Stefan Espant <stefan.van.der.kort@hva.nl>
2023-10-17 10:29:35 +0200

    v0.0.2
    created another page

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Index</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias placeat perferendis ut beatae itaque nostrum iste magnam aperiam doloribus assumenda.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/wensen/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Wensen</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat minus culpa voluptates numquam fugit nemo dignissimos doloribus vero quidem qui?</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
