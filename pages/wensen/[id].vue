40f559d6260085025d5f3aa62013378c1ac35dfd
Stefan Espant <stefan.van.der.kort@hva.nl>
2023-10-17 13:04:16 +0200

    v0.0.3
    set up detailpage with dynamic id

---
+++ pages/wensen/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p> wens {{ id }}</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, quam!</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
