1e43a5e86e59c934403acd78056d50e6fbd70ca8
Stefan Espant <stefan.van.der.kort@hva.nl>
2023-10-18 16:51:47 +0200

    v0.0.6
    big reset

--- nuxt.config.ts
+++
@@ -1,6 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  modules: [
-    ['@storyblok/nuxt', { accessToken: process.env.STORYBLOK_TOKEN }]
-  ]
-})
