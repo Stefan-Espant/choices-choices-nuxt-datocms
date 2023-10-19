e9469cb188e01192d2186e407885f644bbef7eaa
Stefan Espant <stefan.van.der.kort@hva.nl>
2023-10-19 16:50:24 +0200

    v0.0.9
    landingspage donw

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,22 @@
 <template>
     <div>
         <header>
-            <img src="https://hallostrandeiland.nl/blobs/gebiedsplatform/w250h250/98691/2022/50/hseTekengebied_8.png" alt="Logo van Hallostrandeiland" />
+            <nav class="top-nav">
+                <ul>
+                    <li>
+                        <NuxtLink to="/">
+                            Iets plaatsen 
+                        </NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/">
+                            Inloggen of registreren
+                        </NuxtLink>
+                    </li>
+                </ul>
+            </nav>
             <nav>
-
+                <img src="https://hallostrandeiland.nl/blobs/gebiedsplatform/w250h250/98691/2022/50/hseTekengebied_8.png" alt="Logo van Hallostrandeiland" />
             </nav>
         </header>
         <!-- Pagina content -->
@@ -20,11 +33,53 @@
 <style scoped>
     header {
         width: 100vw;
-        height: 100px;
+        height: auto;
+    }
+
+    /* Instellingen voor eerste nav */
+    header nav:first-child {
+        background-color: var(--color-primary-50);
+        padding: var(--unit-small) var(--unit-small) var(--unit-default) var(--unit-small);
+        overflow: auto;
+        padding: var(--unit-small) var(--unit-default) ;
+    }
+
+    header nav:first-child ul {
+        width: max-content;
+        display: flex;
+        flex-direction: row;
+        gap: var(--unit-default);
+        margin-left: auto;
+        padding: 0 var(--unit-default);
+    }
+
+    header nav:first-child a {
+        color: var(--color-secundary-pure);
+        transition: var(--animation-quick);
+        padding: var(--unit-micro) var(--unit-small);
+        border-radius: var(--unit-micro);
+    }
+
+    header nav:first-child a:is(:hover, :focus, :active) {
+        background-color: var(--color-secundary-pure);
+        color: var(--color-primary-50);
+    }
+
+    /* Instellingen voor tweede nav */
+    header nav:last-child {
+        width: 100%;
+        background-color: var(--color-secundary-pure);
+        position: sticky;
     }
 
     img {
-        width: 100px;
+        width: 84px;
         aspect-ratio: 1/1;
+    }
+
+    @media (min-width: 27.5rem){
+        header nav:first-child ul {
+            padding: 0;
+        }
     }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,22 @@
 <template>
         <section class="hero">
-            <h1>
-                Welkom thuis op strandeiland
-            </h1>
+            <div>
+            <article>
+                <h1>
+                    Welkom thuis op strandeiland
+                </h1>
+            </article>
+            <article>
+                <p>
+                Het land van Strandeiland ligt klaar! Het land moet nu zakken, tot rust komen en vervolgens kan er gebouwd gaan worden. Op deze website kan je het laatste nieuws over Strandeiland volgen en deelnemen aan het gesprek over hoe we van dit eiland een prachtige plek kunnen maken om te wonen, werken en ontspannen. 
+                </p>
+                <NuxtLink to="/wensen">Ga naar wensen</NuxtLink>
+            </article>
+            </div>
         </section>
 
-    <p v-if="error">Something bad happened!</p>
-    <p v-else>Data: <code>{{ data }}</code></p>
+    <!-- <p v-if="error">Something bad happened!</p>
+    <p v-else>Data: <code>{{ data }}</code></p> -->
 </template>
 
 <script setup>
@@ -72,10 +82,91 @@ definePageMeta({
     layout: 'default'
 })
 
+
 </script>
 
 <style scoped>
     .hero {
         width: 100%;
+        height: 100vh;
+        background-image: url(https://hallostrandeiland.nl/networks/hallostrandeiland/img/hse_hero-bg.avif);
+        background-size: cover;
+        background-position: center;
     }
+
+    .hero div {
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .hero div article{
+        width: fit-content;
+        padding: var(--unit-default);
+        margin: 0 auto;
+    }
+
+    .hero div article:last-child {
+        height: fit-content;
+        background-color: var(--color-branding-secundary);
+        padding: var(--unit-default);
+    }
+
+    .hero div article:last-child a {
+        position: relative;
+        color: var(--color-primary);
+        transition: var(--animation-quick) ease-in-out;
+        padding: var(--unit-small);
+        border-bottom: 1px solid var(--color-branding-secundary);
+    }
+
+    .hero div article:last-child a::after {
+        content:' >';
+        position: absolute;
+        right: -1rem;
+    }
+
+    .hero div article:last-child a:is(:hover, :focus) {
+        border-bottom: 1px solid var(--color-primary);
+    }
+
+    .hero div article:last-child a:is(:hover, :focus)::after {
+        right: -1.5rem;
+    }
+
+    .hero div article:first-child h1 {
+        width: fit-content;
+        font-size: var(--unit-large);
+        color: var(--color-secundary-pure);
+        background-color: rgba(0,0,0,0.66);
+        padding: var(--unit-default);
+    }
+    @media (min-width: 30rem){
+        .hero div article:first-child h1 {
+            width: 28rem;
+            font-size: var(--unit-heading);
+        }
+    }
+
+    @media (min-width: 67.5rem){
+        .hero div {
+            width: 80%;
+            display: flex;
+            flex-direction: row;
+            gap: var(--unit-heading);
+            align-items: center;
+            justify-content: center;
+            margin: 0 auto;
+        }  
+        .hero div article:first-child h1 {
+            width: 28rem;
+            font-size: var(--unit-heading);
+        }
+    }
+    
+
+
 </style>
